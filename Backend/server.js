15c74ea20a4d596a60d83d0fdda9ee7fae538931
Suraj <skbanavalikar2105@gmail.com>
2026-06-07 21:45:39 +0530

    1) server template running

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,10 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.listen(port,'0.0.0.0',()=>{
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>');
+});
+
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on port ${port}`);
-})
+});
