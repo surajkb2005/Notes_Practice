ec453800902801527a313edf9d183d43ed3347ca
Suraj <skbanavalikar2105@gmail.com>
2026-07-03 23:13:51 +0530

    4) update and delete and get only read successfully done

--- Backend/server.js
+++ Backend/server.js
@@ -13,12 +13,21 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get('/books',async (req,res)=>{
+app.get('/books', async (req, res) => {
     const notes = await Note.find();
 
     res.status(200).json({
-        message:'Notes fetched successfully',
-        notes:notes
+        message: 'Notes fetched successfully',
+        notes: notes
+    });
+});
+
+app.get('/books/read', async (req, res) => {
+    const notes = await Note.find({ read: true });
+
+    res.status(200).json({
+        message: 'Read notes fetched successfully',
+        notes: notes
     });
 });
 
@@ -34,6 +43,39 @@ app.post('/books', async (req, res) => {
     res.status(200).json({
         message: 'Note created successfully',
         note: newNote
+    });
+});
+
+app.put('/books/:id', async (req, res) => {
+    const id = req.params.id;
+    const { title, content, read } = req.body;
+
+    const updateNote = await Note.findByIdAndUpdate(id, { title, content, read }, { new: true });
+
+    if (!updateNote) {
+        return res.status(404).json({
+            message: 'Note not found'
+        });
+    }
+
+    res.status(200).json({
+        message: 'Note updated successfully',
+        note: updateNote
+    });
+});
+
+app.delete('/books/:id', async (req, res) => {
+    const id = req.params.id;
+    const deletedNote = await Note.findByIdAndDelete(id);
+
+    if (!deletedNote) {
+        return res.status(404).json({
+            message: 'Note not found'
+        });
+    }
+
+    res.status(200).json({
+        message: 'Note deleted successfully'
     });
 });
 
