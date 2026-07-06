24c354c71db15cfd91f7f6ffd91d6643f0fd66ed
Suraj <skbanavalikar2105@gmail.com>
2026-07-06 23:20:53 +0530

    9) Dockerized Frontend properly

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API = "http://localhost:3000/notes";
+const API = "http://localhost:5000/notes";
 
 function App() {
 
