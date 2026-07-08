832c90d76c9424169fac12d608630fda000794bb
Suraj <skbanavalikar2105@gmail.com>
2026-07-08 18:27:40 +0530

    10) Cloud Hosted .....

--- Backend/server.js
+++ Backend/server.js
@@ -20,6 +20,6 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>');
 });
 
-app.listen(port, "0.0.0.0", () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on port ${port}`);
 });
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API = "http://localhost:5000/notes";
+const API = "http://4.193.207.83:5000/notes";
 
 function App() {
 
